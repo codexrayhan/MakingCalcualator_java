b361684adc43fe454f532ff7092ee11f4ff2fb71
Md Rayhan Hossain <hmdrayhan533@gmail.com>
2026-05-19 02:55:45 +0600

    Update and rename main.java to Main.java

--- main.java
+++
@@ -1,7 +0,0 @@
-import javax.swing.*;
-public class Main {
-    public static void main(String[] args) {
-        // Run UI on the Event Dispatch Thread for thread safety
-        SwingUtilities.invokeLater(Calculator::new);
-    }
-}
